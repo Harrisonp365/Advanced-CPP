2a45dc2b04fe5c3dcca262f528c8219d526d3dfa
Harrisonp365 <harrisonpowell365@gmail.com>
2021-01-20 09:41:44 +1100

    built out a simple vector class

--- Vector and Free store/vectors/Main.cpp
+++ Vector and Free store/vectors/Main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
@@ -7,11 +6,14 @@ class vector {
 public:
 	vector(int s) : sz{ s }, elem{ new double[s] }  // constructor allocate s double, stores s in sz
 	{
-		for (int i = 0; i < s; ++i) elm[i] = 0;
+		for (int i = 0; i < s; ++i) elem[i] = 0;
 	} 
-
+	
+	~vector() { delete[] elem; } //destructor
 	int size() const { return sz; } // current size
 
+	double get(int n) const { return elem[n]; } //read
+	void set(int n, double v) { elem[n] = v; } // write
 private:
 	int sz; // size
 	double* elem; // pointer to first element (double)
@@ -20,11 +22,25 @@ private:
 
 int main()
 {
+	vector v(5);
+	for (int i = 0; i < v.size(); ++i)
+	{
+		v.set(i, 1.1 * i);
+		std::cout << "v[" << i << "]==" << v.get(i) << "\n";
+	}
 
+	vector* f(int s)
+	{
+		vector* p = new vector(s); // allocate a vector on free storage
+		//fill *p
+		return p;
+	}
+
+	void ff()
+	{
+		vector* q = f(4);
+		// use *q
+		delete q; // free vector on heap 
+	}
 	return 0;
-}
-
-vector::vector(int s)
-{
-
 }
