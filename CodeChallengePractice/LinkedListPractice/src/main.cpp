4f76a88d1adfff881968524405019fa0d120e1cd
Harrisonp365 <harrisonpowell365@gmail.com>
2021-03-09 11:22:27 +1100

    linked list deletion practice using ints and nodes

--- CodeChallengePractice/LinkedListPractice/src/main.cpp
+++ CodeChallengePractice/LinkedListPractice/src/main.cpp
@@ -43,6 +43,34 @@ public:
 		return;
 	}
 
+	void deleteNodeByInt(int intToDelete)
+	{
+		Node* temp = head;
+		Node* prev = head;
+		if (!head)
+			return;
+		if (head->data == intToDelete)
+		{
+			head = temp->next;
+			delete(temp);
+			return;
+		}
+		while (temp != nullptr && temp->data != intToDelete)
+		{
+			prev = temp;
+			temp = temp->next;
+		}
+		if (temp == nullptr)
+		{
+			std::cout << intToDelete << " does not exist\n";
+			return;
+		}
+
+		prev->next = temp->next;
+		std::cout << intToDelete << " has been delted\n";
+		delete(temp);
+	}
+
 	void printList()
 	{
 		if (!head)
@@ -67,6 +95,10 @@ int main()
 	Node* n1 = new Node(2);
 	myList.addNodeWithNode(n1);
 	myList.addNodeWithInt(777);
+	myList.addNodeWithInt(125);
+	myList.printList();
+	myList.deleteNodeByInt(7);
+	myList.deleteNodeByInt(2);
 	myList.printList();
 	return 0;
 }
