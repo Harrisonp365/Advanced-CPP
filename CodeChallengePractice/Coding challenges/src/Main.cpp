baadcea9aef2f7e7192c6dd2e859761d0e7b8a88
Harrisonp365 <harrisonpowell365@gmail.com>
2021-04-23 14:09:10 +1000

    more code challenge practice

--- CodeChallengePractice/Coding challenges/src/Main.cpp
+++ CodeChallengePractice/Coding challenges/src/Main.cpp
@@ -361,5 +361,71 @@ int hourGlassSum(vector<vector<int>>arr)
 }
 
 same main loop;
+------------------------------------------------------------------------------------------------
+////////////////////////////////STUDENT INHERITS PERSON CLASS///////////////////////////////////
+
+Class Person{
+	protected:
+		string firstName;
+		string lastName;
+		int id;
+	public:
+		Person(string firstName, string lastName, int identification){
+			this->firstName = firstName;
+			this->lastName = lastName;
+			this->id = identification;
+		}
+		void printPerson(){
+			cout<< "Name: "<< lastName << ", "<< firstName <<"\nID: "<< id << "\n";
+		}
+
+};
+
+class Student :  public Person{
+	private:
+		vector<int> testScores;
+	public:
+
+		Student(string firstname, string lastname, int identification, vector<int> score) :
+		Person(firstname, lastname, identification), testScores(score) {}
+
+		char calculate()
+		{
+			int average = 0;
+
+			for(int i = 0; i < testScores.size(); i++)
+			{
+				average += testScores[i];
+			}
+			average = average/testScores.size();
+
+			if(average >= 90)
+			{
+				return 'O';
+			}
+			else if(average >= 80 && average < 90)
+			{
+				return 'E';
+			}
+			else if(average >= 70 && average < 80)
+			{
+				return 'A';
+			}
+			else if(average >= 55 && average < 70)
+			{
+				return 'P';
+			}
+			else if(average >= 40 && average < 55)
+			{
+				return 'D';
+			}
+			else
+			{
+				return 'T';
+			}
+		}
+};
+-------------------------------------------------------------------------------------------------
+/////////////////////////////////
 */
 
