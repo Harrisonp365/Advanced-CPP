46032214bc22381776a4f3bd1620b648b56972c8
Harrisonp365 <harrisonpowell365@gmail.com>
2021-05-18 14:28:03 +1000

    made code for insert nicers using a pointer reference and also implimented a contains function

--- CodeChallengePractice/BST/BST.cpp
+++ CodeChallengePractice/BST/BST.cpp
@@ -19,27 +19,25 @@ public:
 
     void print();
     void insert(int val);
+    bool contains(int val);
 
 private:
     TreeNode* root;
-    std::string subTreeAsString(TreeNode* node); //Helper func for print
-    void insert(int val, TreeNode* node); // Helper func for insert
+    std::string subTreeAsString(TreeNode* node); //Helper func for print()
+    void insert(int val, TreeNode*& node); // Helper func for insert()
+    bool contains(int val, TreeNode*& node); // Helper func for contains()
 };
 
 int main()
 {
-    //create and empty tree
     BSTree myTree{};
-    myTree.print();
-   
     myTree.insert(5);
-    myTree.print();
-
     myTree.insert(4);
-    myTree.print();
-
     myTree.insert(6);
-    myTree.print();
+
+    std::cout << std::boolalpha << myTree.contains(4) << std::endl;
+    std::cout << std::boolalpha << myTree.contains(2) << std::endl;
+    
 
     return 0;
 }
@@ -58,10 +56,12 @@ void BSTree::print()
 
 void BSTree::insert(int val)
 {
-    if (root == nullptr)
-        this->root = new TreeNode(val);
-    else
-        this->insert(val, this->root);
+    this->insert(val, this->root);
+}
+
+bool BSTree::contains(int val)
+{
+    return contains(val, this->root);
 }
 
 std::string BSTree::subTreeAsString(TreeNode* node)
@@ -71,38 +71,51 @@ std::string BSTree::subTreeAsString(TreeNode* node)
     std::string result = "{" + std::to_string(node->data) + ", " + leftString + ", " + rightString + "}";
     return result;
 }
-//Insert a new node into subtree starting at node
-void BSTree::insert(int val, TreeNode* node)
-{
-    //Check if new nodes value = val if so warn and exit func
-    if (val == node->data)
-    {
-        std::cout << "warning val already exists, will now exit function";
-        return;
-    }
 
-    if (val < node->data)
+void BSTree::insert(int val, TreeNode*& node)
+{
+    if (node == nullptr) 
     {
-        if (node->left == nullptr)
+        // Case: node is a nullptr
+        // Make a new TreeNode for it to point to
+        node = new TreeNode(val);
+    }
+    else 
+    {
+        if (val < node->data) 
         {
-            //make a new node as the left child of this node
-            node->left = new TreeNode(val);
-        }
-        else
-        {
-            //recursively call insert() on nodes left child
+            // Case: val is < node's data
             this->insert(val, node->left);
         }
+        else if (val > node->data) 
+        {
+            // Case: val is > node's data
+            this->insert(val, node->right);
+        }
+        else 
+        {
+            // Case: val is equal to node's data
+            std::cout << "Warning: Value already exists, so nothing will be done." << std::endl;
+        }
+    }
+}
+
+bool BSTree::contains(int val, TreeNode*& node)
+{
+    if (node == nullptr)
+    {
+        return false;
+    }
+    else if (val == node->data)
+    {
+        return true;
+    }
+    else if (val < node->data)
+    {
+        return this->contains(val, node->left);
     }
     else
     {
-        if (node->right == nullptr)
-        {
-            node->right = new TreeNode(val);
-        }
-        else
-        {
-            this->insert(val, node->right);
-        }
+        return this->contains(val, node->right);
     }
 }
