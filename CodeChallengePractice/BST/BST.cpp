fbaef32d4d88d251bb475893ea82d6f877fdcdac
Harrisonp365 <harrisonpowell365@gmail.com>
2021-05-18 14:16:13 +1000

    There is now a simple insert function I think this could be made simpler though

--- CodeChallengePractice/BST/BST.cpp
+++ CodeChallengePractice/BST/BST.cpp
@@ -18,30 +18,30 @@ public:
     BSTree(TreeNode* rootNode): root(rootNode) {}
 
     void print();
+    void insert(int val);
 
 private:
     TreeNode* root;
     std::string subTreeAsString(TreeNode* node); //Helper func for print
+    void insert(int val, TreeNode* node); // Helper func for insert
 };
 
 int main()
 {
     //create and empty tree
-    BSTree emptyTree{};
-    emptyTree.print();
-    //create a tree with 4 left child and 6 right child
-
-    TreeNode root(5);
-    TreeNode leftChild(4);
-    TreeNode rightChild(6);
-
-    //connect Nodes
-    root.left = &leftChild;
-    root.right = &rightChild;
-
-    BSTree myTree{ &root };
+    BSTree myTree{};
+    myTree.print();
+   
+    myTree.insert(5);
     myTree.print();
 
+    myTree.insert(4);
+    myTree.print();
+
+    myTree.insert(6);
+    myTree.print();
+
+    return 0;
 }
 
 void BSTree::print()
@@ -56,10 +56,53 @@ void BSTree::print()
     }
 }
 
+void BSTree::insert(int val)
+{
+    if (root == nullptr)
+        this->root = new TreeNode(val);
+    else
+        this->insert(val, this->root);
+}
+
 std::string BSTree::subTreeAsString(TreeNode* node)
 {
     std::string leftString = (node->left == nullptr) ? "{}" : subTreeAsString(node->left);
     std::string rightString = (node->right == nullptr) ? "{}" : subTreeAsString(node->right);
     std::string result = "{" + std::to_string(node->data) + ", " + leftString + ", " + rightString + "}";
     return result;
+}
+//Insert a new node into subtree starting at node
+void BSTree::insert(int val, TreeNode* node)
+{
+    //Check if new nodes value = val if so warn and exit func
+    if (val == node->data)
+    {
+        std::cout << "warning val already exists, will now exit function";
+        return;
+    }
+
+    if (val < node->data)
+    {
+        if (node->left == nullptr)
+        {
+            //make a new node as the left child of this node
+            node->left = new TreeNode(val);
+        }
+        else
+        {
+            //recursively call insert() on nodes left child
+            this->insert(val, node->left);
+        }
+    }
+    else
+    {
+        if (node->right == nullptr)
+        {
+            node->right = new TreeNode(val);
+        }
+        else
+        {
+            this->insert(val, node->right);
+        }
+    }
 }
