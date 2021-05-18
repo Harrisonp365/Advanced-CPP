e6102c944aea65658f4d2307ab8f960f132ec5e0
Harrisonp365 <harrisonpowell365@gmail.com>
2021-05-18 13:56:38 +1000

    a print function has been added and the ability to init an empty tree

--- CodeChallengePractice/BST/BST.cpp
+++ CodeChallengePractice/BST/BST.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>   
 
 struct TreeNode
 {
@@ -9,9 +10,25 @@ struct TreeNode
     TreeNode(int data) : data(data), left(nullptr), right(nullptr) {}
 };
 
+class BSTree
+{
+public:
+    //constructors
+    BSTree() : root(nullptr) {}
+    BSTree(TreeNode* rootNode): root(rootNode) {}
+
+    void print();
+
+private:
+    TreeNode* root;
+    std::string subTreeAsString(TreeNode* node); //Helper func for print
+};
 
 int main()
 {
+    //create and empty tree
+    BSTree emptyTree{};
+    emptyTree.print();
     //create a tree with 4 left child and 6 right child
 
     TreeNode root(5);
@@ -22,6 +39,27 @@ int main()
     root.left = &leftChild;
     root.right = &rightChild;
 
-    // Print info about foo
-    std::cout << "data: " << root.data << ", left: " << root.left->data << ", right: " << root.right->data << std::endl;
+    BSTree myTree{ &root };
+    myTree.print();
+
+}
+
+void BSTree::print()
+{
+    if (this->root == nullptr)
+    {
+        std::cout << "{}" << std::endl;
+    }
+    else
+    {
+        std::cout << this->subTreeAsString(this->root) << std::endl;
+    }
+}
+
+std::string BSTree::subTreeAsString(TreeNode* node)
+{
+    std::string leftString = (node->left == nullptr) ? "{}" : subTreeAsString(node->left);
+    std::string rightString = (node->right == nullptr) ? "{}" : subTreeAsString(node->right);
+    std::string result = "{" + std::to_string(node->data) + ", " + leftString + ", " + rightString + "}";
+    return result;
 }
